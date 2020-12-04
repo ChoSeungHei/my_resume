f908abfaf5cc8884ce39df4a4cf8abedc457a2cf
ChoSeungHei <kwa0403261@gmail.com>
2020-12-04 15:01:23 +0900

    feat: fix project

--- src/assets/project.js
+++ src/assets/project.js
@@ -1,21 +1,27 @@
 const projects = [
     {
         uuid: 0, 
-        heading: '광고 서비스 관리 플랫폼 개발',
+        heading: '여행 타겟팅 광고 관리 플랫폼 개발',
         period: '2019.12 - 2020.11',
-        content: '- 광고 업로드/검증 프론트엔드 개발[React]\n- 광고 사용량 대쉬보드 개발[Amazon Elastic Search]\n- 서비스 플랫폼 배포[AWS Amplify, Git]'
+        content: '- 여행 상태, 관심 여행지등의 타겟팅을 반영한 광고 등록 및 검증 기능 개발 [ React ]\n- 광고 클릭수, 전일 대비 광고 추이, 전체 매출 대시보드 개발\n- 서비스 플랫폼 배포[AWS Amplify, Git]'
     },
     {
         uuid: 1, 
-        heading: '블록체인 광고 보상 애플리케이션 개발 및 운영',
+        heading: '블록체인 광고 보상 애플리케이션 개발',
         period: '2020.02 - 2020.05',
-        content: '- 블록체인 애플리케이션 (페이웍) 프론트엔드 개발[React Native]\n- 광고 보상 기능 개발 및 운영'
+        content: '-광고 참여시 블록체인 토큰을 보상하는 애플리케이션(페이웍) 프론트 엔드 개발[ ReactNative ]\n- 블록체인 토큰을 기프티콘으로 교환 프론트엔드 개발'
     },
     {
         uuid: 2, 
+        heading: '카드 추천 웹앱 개발',
+        period: '2019.09 - 2019.10',
+        content: '- 소유한 카드의 업종별 단발성 할인을 알려주는 웹앱 프론트엔드 개발 [ PHP, HTML5 ]\n- 관리자용 웹페이지 개발 [PHP, BootStrap]'
+    },
+    {
+        uuid: 3, 
         heading: '사내 홈페이지 개발',
         period: '2019.09 - 2019.10',
-        content: '- 사내 홈페이지 프론트엔드 개발(HTML5)\n- 홈페이지 배포(Elastic Beanstalk, Docker)'
+        content: '- 사내 홈페이지 프론트엔드 개발[ HTML5, CSS, JavaScript ]\n- 홈페이지 배포(Elastic Beanstalk, Docker)'
     },
 ];
 

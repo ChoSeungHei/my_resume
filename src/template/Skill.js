e153832c77928dab68e5e9b68c8fea71a52da3a9
ChoSeungHei <kwa0403261@gmail.com>
2022-06-14 21:34:37 +0900

    fix

--- src/template/Skill.js
+++ src/template/Skill.js
@@ -16,7 +16,7 @@ const Skill = () => {
                 Back-end
             </div>
             <div className="section_text2">
-                PHP, Node.js, AWS(EC2, S3, RDS, Am)
+                PHP, Node.js, AWS(EC2, S3, RDS, Amplify)
             </div>
             <div className="section_mid_title">
                 DataBase
